f6695b6ce585b540c414ba86c94c15f717714682
Eren Amanturov <amanturoveren29@gmail.com>
2024-10-05 18:51:35 +0600

    added chat with channels but without User

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'user.User'
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,6 +60,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+ASGI_APPLICATION = 'config.asgi.application'
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 DATABASES = {
     'default': {
